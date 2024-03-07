27f92da831cd75f6a72a61be2cf0818fe604b22d
HalehEsmaeili <131443154+HalehEsmaeili@users.noreply.github.com>
2024-03-07 14:38:53 +0100

    fic

--- client/src/Api/axiosInstance.js
+++ client/src/Api/axiosInstance.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import dotenv from "dotenv";
-dotenv.config({ path: `${__dirname}/.env` });
+
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'https://passtilliosrepository.onrender.com',
 });
