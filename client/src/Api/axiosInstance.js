76db2ff478fb08d2ae179c0c48d4dcc911209ac5
HalehEsmaeili <131443154+HalehEsmaeili@users.noreply.github.com>
2024-03-07 14:32:14 +0100

    change base url in axiosInstance for deployment

--- client/src/Api/axiosInstance.js
+++ client/src/Api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-
+import dotenv from "dotenv";
+dotenv.config({ path: `${__dirname}/.env` });
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3001',
+  baseURL: process.env.REACT_APP_API_URL || 'https://passtilliosrepository.onrender.com',
 });
 
 export default axiosInstance;
