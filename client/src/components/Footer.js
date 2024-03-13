c620d089c9827252a5a9a934fef3eb58338b3b20
HalehEsmaeili <131443154+HalehEsmaeili@users.noreply.github.com>
2024-03-13 11:08:19 +0100

    minor changes to css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -12,13 +12,13 @@ function Footer() {
     return (
       <div style={{width:"80%", marginTop: "10%", textAlign: 'center', marginBottom: 0, display: 'flex', justifyContent: 'center',position:"relative" ,marginLeft:"10%"}}>
       <Link to="/Impressum" className="button" style={{  }}>
-        <p style={{ position:"relative",display: 'inline-block', textAlign: 'center',color:"#00eeb9",marginLeft:"30%",fontSize:"1.2em"  }} className="mt-5 mb-3 text-body-secondary">Impressum</p>
+        <p style={{ position:"relative",display: 'inline-block', textAlign: 'center',color:"#00eeb9",marginLeft:"30%",fontSize:"2vw"  }} className="mt-5 mb-3 text-body-secondary">Impressum</p>
       </Link>
      
       <p style={{display: 'inline-block',paddingLeft:"4%", textAlign: 'center', color: 'white' }} className="mt-5 mb-3 text-body-secondary">&copy; passtillios 2022</p>
     
       <Link to="/Datenschutz" className="button" style={{  }}>
-        <p style={{ display: 'inline-block', textAlign: 'center',fontSize:"1.2em",color:"#00eeb9" }} className="mt-5 mb-3 text-body-secondary">Datenschutz</p>
+        <p style={{ display: 'inline-block', textAlign: 'center',fontSize:"2vw",color:"#00eeb9" }} className="mt-5 mb-3 text-body-secondary">Datenschutz</p>
       </Link>
     </div>
     );
