cabe6b145819b3884491f9e9eab13db24d663c57
HalehEsmaeili <131443154+HalehEsmaeili@users.noreply.github.com>
2024-04-03 13:58:28 +0200

    add dataprivacy translations

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
+import "./footer.css";
 function Footer() {
 
     const navigate= useNavigate();
@@ -14,18 +14,30 @@ function Footer() {
     };
   
     return (
-      <div style={{width:"80%", marginTop: "10%", textAlign: 'center', marginBottom: 0, display: 'flex', justifyContent: 'center',position:"relative" ,marginLeft:"10%"}}>
      
+     
+     <div style={{width:"100%",marginTop:"10%", textAlign: 'center', marginBottom: 0, display: 'flex', justifyContent: 'center',position:"relative" }}>
+     
+     <footer className="footer">
+      <div className="footer-content">
+        <span onClick={()=>navigate("/impressum")}  className="footer-link">Impressum</span>
+        <span className="separator">|</span>
+        <span onClick={()=>navigate("/datenschutz")}  className="footer-link">Data Policy</span>
+        <span className="separator" >|</span>
+        <span style={{margin:"0 10px"}} className="separator"> &copy; passtillios 2022 </span>
+      </div>
+
+    </footer>
+{/*
         <button name='Impressum' onClick={()=>navigate("/impressum")} style={{ position:"relative",display: 'inline-block', textAlign: 'center',color:"#00eeb9",marginLeft:"30%",fontSize:"2vw"  }} className="mt-5 mb-3 text-body-secondary">Impressum</button>
        
-       
-
-     
+    
       <p style={{display: 'inline-block',paddingLeft:"4%", textAlign: 'center', color: 'white' }} className="mt-5 mb-3 text-body-secondary">&copy; passtillios 2022</p>
     
       <Link to="/Datenschutz" className="button" style={{  }}>
         <p style={{ display: 'inline-block', textAlign: 'center',fontSize:"2vw",color:"#00eeb9" }} className="mt-5 mb-3 text-body-secondary">Datenschutz</p>
       </Link>
+      */}
     </div>
     );
   }
--- client/src/pages/Impressum.jsx
+++ client/src/pages/Impressum.jsx
@@ -1,6 +1,6 @@
 
 import React, { useEffect, useState } from "react";
-
+import NextSteps from "../components/NextSteps";
 
 //import "./TeamPage.css";
 import "./ContactPage.css";
@@ -28,7 +28,21 @@ beckstrasse 8<br />
 {/*Telefax: +49 (0) 123 44 55 99<br />*/}
 E-Mail: haleh@passtillios.com</p>
 
-
+<div
+          style={{
+            zIndex: 0,
+            position: "relative",
+            marginTop: "-80%",
+            marginBottom: "0%",
+            overflow:"hidden"
+          }}
+        >
+          <NextSteps
+            dontshow="btn3"
+            currentStation="contact"
+            conditionForAnimStart={true}
+          />
+        </div>
     </div>
   );
 };
