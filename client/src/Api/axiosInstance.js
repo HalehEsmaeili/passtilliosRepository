cccd0c3b4bd76e3a1569a9f5e26fb6497478a532
HalehEsmaeili <131443154+HalehEsmaeili@users.noreply.github.com>
2024-05-02 16:32:53 +0200

    update the api axiosinstance

--- client/src/Api/axiosInstance.js
+++ client/src/Api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:'http://localhost:3004',
+  baseURL:process.env.REACT_APP_API_URL,
 });
 //baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3004',
 
